92d50846f4054e2b528c8c8404ad575dd13de661
Ayush <yorksdev@gmail.com>
2020-10-30 00:08:14 +0530

    C Assignment 6 and C++ Section 7

---
+++ C/UTA FALL 2020 Assignments/Assignment 6/encoder.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <string.h>
+#define MAX 1000
+
+int main() {
+	
+	printf("Enter a string: ");
+	char string[MAX];
+	fgets(string, MAX, stdin);
+
+	//  strrchr -> gives pointer to last occurance of char.
+	
+	int len = (int)(strrchr(string, '\n') - string);
+	string[len] = '\0';	
+	
+	for(int i = 0; i <= len - 1; i++) {
+		int new_char = string[i] + 3;	
+		if(new_char > 127) {
+			new_char = 31 + (new_char - 127);
+		}
+		string[i] = new_char;
+	}
+	printf("%s", string);
+}
