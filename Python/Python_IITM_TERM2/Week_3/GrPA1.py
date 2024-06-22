9c01056dba09535c1364e940e77b3e786da30fc3
Ayush Dutta <yorksdev@gmail.com>
2024-06-22 19:27:47 +0800

    week 3 IITM Python GrPA

--- Python/Python_IITM_TERM2/Week_3/GrPA1.py
+++ Python/Python_IITM_TERM2/Week_3/GrPA1.py
@@ -13,3 +13,101 @@
 # only_even_squares: Continuously read numbers from standard input until "NAN" is encountered. Print the square of each number only if it is even.
 # Filter and Accumulate - Accumulating a result with selected items
 # only_odd_lines: Continuously read lines from standard input until "END"(not included in the output) is encountered. Create a string by prepending only the odd lines (starting from 1) with a newline character in between, and print the result which will be the odd lines in reverse order.
+
+# Note this prefix code is to verify that you are not using any for loops in this exercise. This won't affect any other functionality of the program.
+with open(__file__) as f:
+    content = f.read().split("# <eoi>")[2]
+if "for " in content:
+    print("You should not use for loop or the word for anywhere in this exercise")
+
+# This is the first line of the exercise
+task = input()
+# <eoi>
+
+if task == "sum_until_0":
+    total = 0
+    n = int(input())
+    while n != 0:  # the terminal condition
+        total += n  # add n to the total
+        n = int(input())  # take the next n form the input
+    print(total)
+
+elif task == "total_price":
+    total_price = 0
+    while True:  # repeat forever since we are breaking inside
+        line = input()
+        if line == "END":  # The terminal condition
+            break
+        quantity, price = line.split()  # split uses space by default
+        quantity, price = int(quantity), int(price)  # convert to ints
+        total_price += quantity * price  # accumulate the total price
+    print(total_price)
+
+elif task == "only_ed_or_ing":
+    word_list = []
+    word = input()
+    while word.upper() != "STOP":
+        if word.lower().endswith(("ed", "ing")):
+            word_list.append(word)
+
+        word = input()
+    print(*word_list, sep="\n")
+
+elif task == "reverse_sum_palindrome":
+    number_list = []
+    num = input()
+    while int(num) != -1:
+        sum_num = int(num) + int(num[-1::-1])
+        if str(sum_num) == str(sum_num)[::-1]:
+            number_list.append(num)
+        num = input()
+
+    print(*number_list, sep="\n")
+
+elif task == "double_string":
+    line = input()
+    line_list = []
+
+    while len(line) != 0:
+        line_list.append(line * 2)
+
+        line = input()
+
+    print(*line_list, sep="\n")
+
+elif task == "odd_char":
+    line = input()
+    line_list = []
+
+    while True:
+        line_list.append(line[0::2])
+        if line.endswith("."):
+            break
+
+        line = input()
+
+    print(*line_list, sep=" ")
+
+elif task == "only_even_squares":
+    line = input()
+    even_square_list = []
+
+    while line != "NAN":
+        if (even_square := (int(line) ** 2)) % 2 == 0:
+            even_square_list.append(even_square)
+
+        line = input()
+
+    print(*even_square_list, sep="\n")
+
+elif task == "only_odd_lines":
+    line = input()
+    line_list = []
+    i = 1
+    while line != "END":
+        if i % 2 != 0:
+            line_list = [line] + line_list
+
+        i += 1
+        line = input()
+    print(*line_list, sep="\n")
