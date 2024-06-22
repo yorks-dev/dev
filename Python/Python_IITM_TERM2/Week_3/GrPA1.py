e43b257e6ac56a7d9e85bf23e568850a0e50ba64
Ayush Dutta <yorksdev@gmail.com>
2024-06-22 16:13:20 +0800

    week 3 IITM Python

---
+++ Python/Python_IITM_TERM2/Week_3/GrPA1.py
@@ -0,0 +1,15 @@
+# Tasks
+
+# Accumulation - Accumulating a final result
+# sum_until_0: Continuously read integers from standard input until you receive a zero. Print the sum of these integers.
+# total_price: Continuously read pairs of integers from standard input, representing the quantity and price of items, until you receive the string "END". Print the total price of all items.
+# Filtering - Selecting based on a criterion
+# only_ed_or_ing: Continuously read strings from standard input until you encounter the word "STOP" (case insensitive and not included in the output). Print only those strings that end with "ed" or "ing" (case insensitive).
+# reverse_sum_palindrome: Continuously read positive integers from standard input until you encounter a "-1"(not included in the output). Print only those integers for which the sum of the number and its reverse is a palindrome.
+# Mapping - Applying the same operation to different items
+# double_string: Continuously read lines from standard input until an empty line is encountered. Print each line repeated twice.
+# odd_char: Continuously read strings from standard input until you encounter a string ending with a "."(include that string with the "." in the output). Extract characters at odd positions (starting from 1) of each line, and print the results in a single line separated by spaces.
+# Filter and Map - Applying an operation to selected items
+# only_even_squares: Continuously read numbers from standard input until "NAN" is encountered. Print the square of each number only if it is even.
+# Filter and Accumulate - Accumulating a result with selected items
+# only_odd_lines: Continuously read lines from standard input until "END"(not included in the output) is encountered. Create a string by prepending only the odd lines (starting from 1) with a newline character in between, and print the result which will be the odd lines in reverse order.
