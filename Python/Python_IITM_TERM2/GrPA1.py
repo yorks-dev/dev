b3af07bd2fe0baffafe884488975c9339fe7506f
Ayush Dutta <yorksdev@gmail.com>
2024-06-23 18:53:40 +0800

    Removing IITM Python and moving it to IITM-DS

--- Python/Python_IITM_TERM2/GrPA1.py
+++
@@ -1,7 +0,0 @@
-# Circular rotation variables
-
-x1, x2, x3 = 5, 6, 7
-
-x1, x2, x3 = x2, x3, x1
-
-print(x1, x2, x3)
--- Python/Python_IITM_TERM2/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-a = np.array([[2, 3, 5], [1, 2, 3], [1, 1, 2]])
-b = np.array([1, 1, 88])
-x = np.linalg.solve(a, b)
-
-print(x)
